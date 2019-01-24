da23c53448aee1c6a1df1ae1af30ef165923257a
Stanley Ung <stanleyung167@gmail.com>
2019-01-23 18:55:41 -0800

    DOM stuff

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// var body = document.querySelector("body"); // Select the body and store it
+// var isBlue = false;
+
+// // Manipulate the body
+// setInterval(function() {
+//     if (isBlue) {
+//         body.style.background = "white";
+//     } else {
+//         body.style.background = "blue";
+//     }
+//     isBlue = !isBlue;
+// }, 
+// 1000);
