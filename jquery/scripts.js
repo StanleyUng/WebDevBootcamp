c2eda165880b78894be613909427bf22d88bf8df
Stanley Ung <stanleyung167@gmail.com>
2019-02-10 23:50:54 -0800

    jquery stuff

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -1,1 +1,15 @@
-document
+if (!jQuery) {
+    alert("missing jQuery ");
+}
+
+var styles = {
+    color : "red",
+    background: "pink",
+    border: "1px solid red"
+};
+
+// $("h1").css(styles);
+
+// $("div").css("background", "purple");
+// $(".highlight").css("width", "200px");
+// $("#third").css("border", "1px solid orange");
