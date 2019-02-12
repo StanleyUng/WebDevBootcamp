febba25322579d04f7e5723e40801641351d20f2
Stanley Ung <stanleyung167@gmail.com>
2019-02-11 18:59:53 -0800

    added features

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -8,8 +8,34 @@ var styles = {
     border: "1px solid red"
 };
 
-// $("h1").css(styles);
+// Debug stuff
+// $("li").click(function() {
+//     alert("Clicked");
+// });
 
+// $("h1").css(styles);
 // $("div").css("background", "purple");
 // $(".highlight").css("width", "200px");
 // $("#third").css("border", "1px solid orange");
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    $("#list").append("<li>" + $("#item").val() + "</li>");
+});
+
+
+$("li").hover(
+    function() {
+        $(this).css("margin", "7px");
+    },
+    function() {
+        $(this).css("margin", "auto");
+    }
+);
+
+var item = $("li input");
+
+if (item.is(":checked")) {
+    
+}
